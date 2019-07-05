37f5f162f089b5e457cb4965fca6cc0b7400d642
fumitsu-511 <otsuka.fumiya@fujitsu.com>
2019-07-05 16:54:30 +0900

    Initial commit

---
+++ app/src/main/java/otsuka/fumiya/techacademy/calcapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package otsuka.fumiya.techacademy.calcapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addition.setOnClickListener(this)
+        subtraction.setOnClickListener(this)
+        multiplication.setOnClickListener(this)
+        division.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        val intent = Intent(this, SubActivity::class.java)
+        //Log.d("test",input1.text.toString())
+        intent.putExtra("para1",input1.text.toString())
+        intent.putExtra("para2",input2.text.toString())
+
+        when(v.id){
+            R.id.addition -> intent.putExtra("para3","addition")
+            R.id.subtraction -> intent.putExtra("para3","subtraction")
+            R.id.multiplication -> intent.putExtra("para3","multiplication")
+            R.id.division -> intent.putExtra("para3","division")
+        }
+        if (input1.text.isEmpty() || input2.text.isEmpty()){
+            errormessage.text="入力値に値を入力してください。"
+        }else{
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/otsuka/fumiya/techacademy/calcapp/SubActivity.kt
@@ -0,0 +1,33 @@
+package otsuka.fumiya.techacademy.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        val para1 = intent.getStringExtra("para1")
+        val para2 = intent.getStringExtra("para2")
+        val para3 = intent.getStringExtra("para3")
+
+        val val1= para1.toDouble()
+        val val2= para2.toDouble()
+        val val3= para3
+
+        calucular(val1,val2,val3)
+    }
+
+    private fun calucular(val1:Double,val2:Double,val3:String){
+        when(val3){
+            "addition" -> ans.text=(val1 + val2).toString()
+            "subtraction" -> ans.text=(val1 - val2).toString()
+            "multiplication" -> ans.text=(val1 * val2).toString()
+            "division" -> ans.text=(val1 / val2).toString()
+
+        }
+    }
+}
